0e90d31a081926dd20273d5a0253cb1850c8416e
kevin <3170335641@qq.com>
2024-11-14 20:53:51 +0800

    项目初始结构创建

---
+++ gulimall-order/src/test/java/com/rainrry/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainrry.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
